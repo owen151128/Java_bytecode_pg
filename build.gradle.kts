0ab55e737627f90fbf350dca05a7f1c09613fed4
owen151128 <owen151128@gmail.com>
2021-11-17 01:24:05 +0900

    Add ow2.asm dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    val asmVersion = "9.2"
+    implementation("org.ow2.asm:asm:$asmVersion")
+    implementation("org.ow2.asm:asm-util:$asmVersion")
+    implementation("org.ow2.asm:asm-tree:$asmVersion")
+    implementation("org.ow2.asm:asm-commons:$asmVersion")
+    implementation("org.ow2.asm:asm-analysis:$asmVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
